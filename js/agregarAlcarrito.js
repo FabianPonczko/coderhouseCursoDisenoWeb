177cab1b3083a78d306f013017c9351bcc800e29
FabianPonczko <fabianponczko@live.com.ar>
2022-08-16 23:36:08 -0300

    Trabajando en el numero del carrico y cantidad de items

--- js/agregarAlcarrito.js
+++ js/agregarAlcarrito.js
@@ -1,15 +1,22 @@
+
+
 const btnDecompra = document.querySelectorAll(".btn-primary")
 const enCarrito = document.querySelector(".encarrito")
 
 const storage1 = localStorage.getItem("carrito")
 const storage = JSON.parse(storage1)
 let contenedorCarrito =[]
-
+let numeroCarrito=0
+storage.forEach(elemento=>{
+    numeroCarrito += elemento.cantidad
+})
+//Actualiza cuantos items hay en el carrito
 if (storage){
-    enCarrito.innerHTML = storage.length
+    enCarrito.innerHTML = numeroCarrito
     contenedorCarrito = storage
 }
 
+//clase padre
 class Carrito {
     constructor(id,imagen,producto,precio,cantidad=1){
         this.id= id
@@ -18,8 +25,11 @@ class Carrito {
         this.precio= precio
         this.cantidad = cantidad
     }
+    
 }
 
+
+//Por cada btn de compra genero un evento click
 btnDecompra.forEach((btn)=>{
     btn.addEventListener("click",(event)=>{
         const cadaBoton = event.target
@@ -29,12 +39,35 @@ btnDecompra.forEach((btn)=>{
        const imagen = contenedor.querySelector(".img-fluid").src
        const precio = contenedor.querySelector(".precio").textContent
        
+       //genero los item del carrito y lo cargo en el contenedor
        const nuevoItem = new Carrito(id,imagen,titulo,precio)
-       contenedorCarrito.push(nuevoItem)
-       enCarrito.innerHTML = contenedorCarrito.length
        
+       for (i = 0 ;i<storage.length;i++){
+        if(storage[i].id==id){
+            storage[i].cantidad +=1
+            numeroCarrito +=1
+       
+       //actualizo el store
        localStorage.setItem("carrito",JSON.stringify(contenedorCarrito))
-        console.log(contenedorCarrito)
+            return     
+        }
+       }
+       
+  
+     // console.log(index)
+        
+
+       contenedorCarrito.push(nuevoItem)
+       localStorage.setItem("carrito",JSON.stringify(contenedorCarrito))
+      
+      
+       storage.forEach(elemento=>{
+        numeroCarrito += elemento.cantidad
+    })
+       enCarrito.innerHTML = numeroCarrito
+       
+       //actualizo el store
+        
     })
    
 })
